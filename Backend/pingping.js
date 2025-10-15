382503e4c039fb6a7f48f195032515a20da41428
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-10-15 17:48:42 +0530

    loged

--- Backend/pingping.js
+++ Backend/pingping.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const https = require('https');
 
-const TARGET_URL = 'https://internetprotocal.onrender.com/';
+const TARGET_URL = 'https://internetprotocal.onrender.com/ping';
 const PING_TIMEOUT = 5000;
 
 const client = axios.create({
@@ -11,16 +11,14 @@ const client = axios.create({
 });
 
 function pingOnce() {
-  client.get(TARGET_URL).catch(() => {}); // ignore errors silently
+  client.get(TARGET_URL).catch(() => {});  
 }
 
 function schedulePing() {
-  const minutes = Math.floor(Math.random() * 7) + 7; // 7–13 min
   setTimeout(() => {
     pingOnce();
     schedulePing();
-  }, minutes * 60 * 1000);
+  }, 5 * 60 * 1000);
 }
 
-// start
 schedulePing();
